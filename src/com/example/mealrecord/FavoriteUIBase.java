10c7881193844313fbb2758111ca696a47e0d407
scobin <ckscobin@gmail.com>
2014-05-08 19:50:02 +0900

    Update FavoriteUIBase.java

--- src/com/example/mealrecord/FavoriteUIBase.java
+++ src/com/example/mealrecord/FavoriteUIBase.java
@@ -3,6 +3,9 @@ package com.example.mealrecord;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+/**
+ * お気に入りの基本クラス
+ */ 
 public class FavoriteUIBase extends UIBase{
     /** 情報リスト*/
 	protected static ArrayList<HashMap<String,String>> mList;
